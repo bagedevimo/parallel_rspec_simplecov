f79e7d676ae718951576a6bd93d406d58cfec701
Ben Anderson <ben@delivereasy.co.nz>
2026-05-19 20:33:40 +1200

    Initial commit
    A basic plugin(?) for parallel_rspec that collates simplecov reports from
    all workers

---
+++ lib/parallel_rspec_simplecov.rb
@@ -0,0 +1,7 @@
+require "parallel_rspec_simplecov/version"
+require "parallel_rspec_simplecov/simplecov"
+
+module ParallelRSpec
+  module SimpleCov
+  end
+end
---
+++ lib/parallel_rspec_simplecov/version.rb
@@ -0,0 +1,5 @@
+module ParallelRSpec
+  module SimpleCov
+    VERSION = "1.0.0"
+  end
+end
